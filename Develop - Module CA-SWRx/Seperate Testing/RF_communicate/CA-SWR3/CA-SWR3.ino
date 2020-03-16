5abf445fabb09a8f69f91aee93416cec7bcec495
Tùng Phan Thanh <48848418+thanhtung2105@users.noreply.github.com>
2020-03-16 10:51:10 +0700

    Update CA-SWR3.ino

--- Develop - Module CA-SWRx/Seperate Testing/RF_communicate/CA-SWR3/CA-SWR3.ino
+++ Develop - Module CA-SWRx/Seperate Testing/RF_communicate/CA-SWR3/CA-SWR3.ino
@@ -1,7 +1,12 @@
-/* In this product - the address (channel) to communicate is define as SWR (convert to decimal) 
-+ 'date make device' + 'product no.' ; In this case, SWR is known as '83 87 82' and add with the date 
-making device for example today is Feb 17th and this is the third product in that day; then the address 
-for this SWR is: const byte address[15] = "83878217022003"  ( 83 87 82 | 17 02 20 | 03 )          
+/* In this product - the address (channel) to communicate is define as <the code of product> (2 degits)
++ <timestamp_of_production> (10 degits). With list product code:
+- CA-SWR: 10;
+- CA-SWR2: 20;
+- CA-SWR3: 30;
+And the timestamp when we create the product, so we have this list:        
+CA-SWR: 101584324363 (12)
+CA-SWR2: 201584324393 (12)
+CA-SWR3: 301584324410 (12)
 
   Product code: 740a8d1e-c649-475e-a270-c5d9a44b40a8
   RF channel (3 button): 83878226022002
@@ -10,14 +15,16 @@ for this SWR is: const byte address[15] = "83878217022003"  ( 83 87 82 | 17 02 2
                 a0087ff7-3613-442f-b6c5-0d5d2f0f1a30
 */
 
+#include <Arduino.h>
 #include <EEPROM.h>
 #include <RF24.h>
 #include <SPI.h>
+using namespace std;
 
 int CE = 9;
 int CSN = 10;
 RF24 radio(CE, CSN);                         //nRF24L01 (CE,CSN) connections PIN
-const byte address[15] = "83878226022003"; //Changeable
+const uint64_t address = 301584324410;       //Changeable
 
 const int button_1 = 5;
 const int button_2 = 6;
